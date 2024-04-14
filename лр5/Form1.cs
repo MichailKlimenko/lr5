6c084742eeb42dae300d0c143c74e8292373e32c
unknown <klimenkomisa1@gmail.com>
2024-04-14 20:03:57 +0300

    lr5

---
+++ лр5/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace лр5
+{
+    public partial class Form1 : Form
+    {
+        private Set set1 = new Set();
+        private Set set2 = new Set();
+        public Form1()
+        {
+            InitializeComponent();
+            set1.Add(1); // Инициализация множества
+            set2.Add(2); // Инициализация множества
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            set1.AddRandom();
+            listBox1.Items.Add("Добавлен случайный элемент в первое множество: " + set1);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            set2.AddRandom();
+            listBox1.Items.Add("Добавлен случайный элемент во второе множество: " + set2);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Set unionSet = set1 + set2;
+            listBox1.Items.Add("Результат объединения: " + unionSet);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            bool result = set1.CompareTo(set2) <= 0;
+            listBox1.Items.Add("Первое множество меньше или равно второму: " + (result ? "Да" : "Нет"));
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int power = (int)set1;
+            listBox1.Items.Add("Мощность первого множества: " + power);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int element = set1[0];
+            listBox1.Items.Add("Первый элемент первого множества: " + element);
+        }
+    }
+}
---
+++ лр5/Set.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Set
+{
+    private List<int> elements = new List<int>();
+    private Random random = new Random();
+
+    public void Add(int element)
+    {
+        if (!elements.Contains(element))
+            elements.Add(element);
+    }
+
+    public void AddRandom()
+    {
+        int randomElement = random.Next(100); // Пример случайного числа от 0 до 99
+        Add(randomElement);
+    }
+
+    public static Set operator +(Set set1, Set set2)
+    {
+        Set resultSet = new Set();
+        resultSet.elements.AddRange(set1.elements);
+        foreach (int element in set2.elements)
+        {
+            resultSet.Add(element);
+        }
+        return resultSet;
+    }
+
+    public int CompareTo(Set other)
+    {
+        if (other == null)
+            return 1;
+        return this.elements.Count.CompareTo(other.elements.Count);
+    }
+
+    public static implicit operator int(Set set)
+    {
+        return set.elements.Count;
+    }
+
+    public int this[int index]
+    {
+        get { return elements[index]; }
+    }
+
+    public int CountElements()
+    {
+        return elements.Count;
+    }
+
+    public override string ToString()
+    {
+        return $"{{ {string.Join(", ", elements)} }} ({elements.Count})";
+    }
+
+    public class Owner
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Organization { get; set; }
+
+        public Owner(int id, string name, string organization)
+        {
+            Id = id;
+            Name = name;
+            Organization = organization;
+        }
+    }
+
+    public class Date
+    {
+        public DateTime CreationDate { get; set; }
+
+        public Date(DateTime creationDate)
+        {
+            CreationDate = creationDate;
+        }
+    }
+
+    public static class StatisticOperation
+    {
+        public static int Sum(Set set)
+        {
+            int sum = 0;
+            foreach (int element in set.elements)
+            {
+                sum += element;
+            }
+            return sum;
+        }
+
+        public static int Difference(Set set)
+        {
+            if (set.elements.Count == 0)
+                return 0;
+            int max = set.elements[0];
+            int min = set.elements[0];
+            foreach (int element in set.elements)
+            {
+                if (element > max)
+                    max = element;
+                if (element < min)
+                    min = element;
+            }
+            return max - min;
+        }
+
+        public static int Count(Set set)
+        {
+            return set.elements.Count;
+        }
+    }
+}
